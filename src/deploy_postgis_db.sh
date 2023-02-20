46b3c1ff3e55ba9a3cc27533ad580f93a2f908ae
maritzee <amby.121192@gmail.com>
2023-02-19 23:33:15 -0600

    ➕ Add volumes to the containers

--- src/deploy_postgis_db.sh
+++ src/deploy_postgis_db.sh
@@ -6,10 +6,15 @@
 # 01 Create a network
 docker network create postgis_net
 
+# 01.1 crea volumen de docker para postgis *No sabemos si solo se crea al inicio
+docker volume create --name=postgis_volume
+docker volume create --name=pgadmin_volume
+
 # 02 Run postgis container and create postgres database
 
 docker run -d \
     --name postgis01 \
+    --volume=postgis_volume:/postgis_data \
     --network=postgis_net \
 	-e POSTGRES_PASSWORD=password \
     -e POSTGRES_DB=metro_cdmx \
@@ -20,6 +25,7 @@ docker run -d \
 
 docker run -d -p 8080:80 \
     --name pgadmin \
+    --volume=pgadmin_volume:/pgadmin_data \
     --network=postgis_net \
     -e PGADMIN_DEFAULT_EMAIL=admin@gmail.com \
     -e PGADMIN_DEFAULT_PASSWORD=pgadmin_password \
