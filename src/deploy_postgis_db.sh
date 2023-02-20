3daca7213c3096f723c16882aa2c77ac3f7518f8
maritzee <amby.121192@gmail.com>
2023-02-19 23:10:04 -0600

    🐛 Fix some bugs

--- src/deploy_postgis_db.sh
+++ src/deploy_postgis_db.sh
@@ -10,17 +10,18 @@ docker network create postgis_net
 
 docker run -d \
     --name postgis01 \
-    --network="postgis_net" \
-    -e POSTGRES_USER=admin \
+    --network=postgis_net \
 	-e POSTGRES_PASSWORD=password \
     -e POSTGRES_DB=metro_cdmx \
     -p 5432:5432 \
-	postgres
+	postgis/postgis
 
 #03 Run pgadmin container for visualizing database
 
 docker run -d -p 8080:80 \
-    --network="postgis_net" \
-    -e PG_ADMIN_DEFAULT_EMAIL=admin@gmail.com \
-    -e PG_ADMIN_DEFAULT_PASSWORD=pgadmin_password \
+    --name pgadmin \
+    --network=postgis_net \
+    -e PGADMIN_DEFAULT_EMAIL=admin@gmail.com \
+    -e PGADMIN_DEFAULT_PASSWORD=pgadmin_password \
     dpage/pgadmin4
+
